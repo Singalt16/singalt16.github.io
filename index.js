cdd0b7ac55824c42eff1206544505f4ea414f9d1
Singalt16 <singalt16@gmail.com>
2019-03-17 19:39:24 -0400

    added skills, interests, about me

--- index.js
+++
@@ -1,8 +0,0 @@
-const resumeModal = document.getElementById("resume-modal");
-function openModal() {
-    resumeModal.style.display = "flex";
-}
-
-function closeModal() {
-    resumeModal.style.display = 'none';
-}
