6a47d7647fb522182b9f8c00a2625e038794b8f7
Singalt16 <singalt16@gmail.com>
2019-03-17 15:34:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const resumeModal = document.getElementById("resume-modal");
+function openModal() {
+    resumeModal.style.display = "flex";
+}
+
+function closeModal() {
+    resumeModal.style.display = 'none';
+}
